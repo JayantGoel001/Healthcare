b37137e56282db29884659fd3bfa104cd5143e66
CormacKrum <jgoel92@gmail.com>
2020-04-17 20:24:01 +0530

    Seventh

--- app/src/main/java/com/example/healthcare/fragments/Location.kt
+++ app/src/main/java/com/example/healthcare/fragments/Location.kt
@@ -1,35 +1,38 @@
 package com.example.healthcare.fragments
 
 import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
 import android.os.Looper.getMainLooper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.healthcare.LocationChangeListeningActivityLocationCallback
 import com.example.healthcare.R
+import com.google.android.material.button.MaterialButton
 import com.mapbox.android.core.location.LocationEngine
 import com.mapbox.android.core.location.LocationEngineProvider
 import com.mapbox.android.core.location.LocationEngineRequest
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.annotations.Marker
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
 import com.mapbox.mapboxsdk.location.modes.CameraMode
 import com.mapbox.mapboxsdk.location.modes.RenderMode
-import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.MapboxMap
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
-import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.maps.*
 
 
-/**
- * A simple [Fragment] subclass.
- */
 @Suppress("DEPRECATION")
-class Location : Fragment(),OnMapReadyCallback, PermissionsListener{
+class Location : Fragment(),OnMapReadyCallback, PermissionsListener,MapboxMap.OnMapClickListener{
     private lateinit var mapView: MapView
     lateinit var mapboxMap: MapboxMap
     private var permissionsManager: PermissionsManager? = null
@@ -37,6 +40,11 @@ class Location : Fragment(),OnMapReadyCallback, PermissionsListener{
     private val callback: LocationChangeListeningActivityLocationCallback = LocationChangeListeningActivityLocationCallback(this)
     private val DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L
     private val DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5
+    private lateinit var startButton:MaterialButton
+    private lateinit var originPosition: Point
+    lateinit var destinationPosition:Point
+    lateinit var destinationMarker:Marker
+    private var originLocation:Location?=null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Mapbox.getInstance(requireContext(), getString(R.string.mapbox_access_token))
@@ -44,7 +52,10 @@ class Location : Fragment(),OnMapReadyCallback, PermissionsListener{
         mapView = view.findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
+        startButton=view.findViewById(R.id.startButton)
+        startButton.setOnClickListener {
 
+        }
         return view
     }
     override fun onMapReady(mapboxMap: MapboxMap) {
@@ -52,47 +63,39 @@ class Location : Fragment(),OnMapReadyCallback, PermissionsListener{
         mapboxMap.setStyle(Style.MAPBOX_STREETS) {
                 style -> enableLocationComponent(style)
         }
+        val uiSettings=mapboxMap.uiSettings
+        uiSettings.isCompassEnabled=true
     }
     private fun enableLocationComponent(loadedMapStyle: Style) {
 // Check if permissions are enabled and if not request
         if (PermissionsManager.areLocationPermissionsGranted(requireContext())) {
 
-// Get an instance of the component
             val locationComponent = mapboxMap.locationComponent
 
-// Set the LocationComponent activation options
             val locationComponentActivationOptions =
                 LocationComponentActivationOptions.builder(requireContext(), loadedMapStyle)
                     .useDefaultLocationEngine(false)
                     .build()
-
-// Activate with the LocationComponentActivationOptions object
             locationComponent.activateLocationComponent(locationComponentActivationOptions)
 
-// Enable to make component visible
             locationComponent.isLocationComponentEnabled = true
-
-// Set the component's camera mode
             locationComponent.cameraMode = CameraMode.TRACKING
-
-// Set the component's render mode
             locationComponent.renderMode = RenderMode.COMPASS
             initLocationEngine()
+            originLocation=locationComponent.lastKnownLocation
+            originLocation?.let { setCameraPosition(it) }
         } else {
             permissionsManager = PermissionsManager(this)
             permissionsManager!!.requestLocationPermissions(requireActivity())
         }
     }
-
-
     override fun onExplanationNeeded(permissionsToExplain: List<String?>?) {
-        Toast.makeText(
-            requireContext(), "User Location Granted",
-            Toast.LENGTH_LONG
-        ).show()
+        Toast.makeText(requireContext(), "User Location Granted", Toast.LENGTH_LONG).show()
     }
-
-
+    fun setCameraPosition(location: Location)
+    {
+        mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude,location.longitude),13.0))
+    }
     @SuppressLint("MissingPermission")
     private fun initLocationEngine() {
         locationEngine = LocationEngineProvider.getBestLocationEngine(requireContext())
@@ -103,10 +106,7 @@ class Location : Fragment(),OnMapReadyCallback, PermissionsListener{
         locationEngine!!.requestLocationUpdates(request, callback, getMainLooper())
         locationEngine!!.getLastLocation(callback)
     }
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String?>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
         permissionsManager!!.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
     override fun onPermissionResult(granted: Boolean) {
@@ -116,33 +116,26 @@ class Location : Fragment(),OnMapReadyCallback, PermissionsListener{
             Toast.makeText(requireContext(), "User Location Permission Not Granted", Toast.LENGTH_LONG).show()
         }
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         super.onViewCreated(view, savedInstanceState)
     }
-
-
     override fun onStart() {
         super.onStart()
         mapView.onStart()
     }
-
     override fun onResume() {
         super.onResume()
         mapView.onResume()
     }
-
     override fun onPause() {
         super.onPause()
         mapView.onPause()
     }
-
     override fun onStop() {
         super.onStop()
         mapView.onStop()
     }
-
     override fun onDestroy() {
         super.onDestroy()
         if (locationEngine != null) {
@@ -150,16 +143,26 @@ class Location : Fragment(),OnMapReadyCallback, PermissionsListener{
         }
         mapView.onDestroy()
     }
-
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
     }
-
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         mapView.onSaveInstanceState(outState)
     }
 
+    override fun onMapClick(point: LatLng): Boolean {
+        destinationMarker.let {
+            mapboxMap.removeMarker(it)
+        }
+        val markerOption=MarkerOptions()
+        destinationMarker=mapboxMap.addMarker(markerOption.position(point))
+        destinationPosition= Point.fromLngLat(point.longitude,point.latitude)
+        originPosition= Point.fromLngLat(originLocation!!.longitude,originLocation!!.latitude)
 
+        startButton.isEnabled=true
+        startButton.setBackgroundResource(R.color.background)
+        return true
+    }
 }
