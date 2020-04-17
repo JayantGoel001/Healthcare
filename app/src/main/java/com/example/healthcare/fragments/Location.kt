10db3d6183923ba9b67a0e1fbb3db6f4f068fef6
CormacKrum <jgoel92@gmail.com>
2020-04-17 07:37:24 +0530

    Sixth

--- app/src/main/java/com/example/healthcare/fragments/Location.kt
+++ app/src/main/java/com/example/healthcare/fragments/Location.kt
@@ -1,19 +1,165 @@
 package com.example.healthcare.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Looper.getMainLooper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.healthcare.LocationChangeListeningActivityLocationCallback
 import com.example.healthcare.R
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.location.LocationEngineRequest
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+
 
 /**
  * A simple [Fragment] subclass.
  */
-class Location : Fragment() {
+@Suppress("DEPRECATION")
+class Location : Fragment(),OnMapReadyCallback, PermissionsListener{
+    private lateinit var mapView: MapView
+    lateinit var mapboxMap: MapboxMap
+    private var permissionsManager: PermissionsManager? = null
+    private var locationEngine: LocationEngine? = null
+    private val callback: LocationChangeListeningActivityLocationCallback = LocationChangeListeningActivityLocationCallback(this)
+    private val DEFAULT_INTERVAL_IN_MILLISECONDS = 1000L
+    private val DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_location, container, false)
+        Mapbox.getInstance(requireContext(), getString(R.string.mapbox_access_token))
+        val view = inflater.inflate(R.layout.fragment_location, container, false)
+        mapView = view.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
+        return view
     }
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+                style -> enableLocationComponent(style)
+        }
+    }
+    private fun enableLocationComponent(loadedMapStyle: Style) {
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(requireContext())) {
+
+// Get an instance of the component
+            val locationComponent = mapboxMap.locationComponent
+
+// Set the LocationComponent activation options
+            val locationComponentActivationOptions =
+                LocationComponentActivationOptions.builder(requireContext(), loadedMapStyle)
+                    .useDefaultLocationEngine(false)
+                    .build()
+
+// Activate with the LocationComponentActivationOptions object
+            locationComponent.activateLocationComponent(locationComponentActivationOptions)
+
+// Enable to make component visible
+            locationComponent.isLocationComponentEnabled = true
+
+// Set the component's camera mode
+            locationComponent.cameraMode = CameraMode.TRACKING
+
+// Set the component's render mode
+            locationComponent.renderMode = RenderMode.COMPASS
+            initLocationEngine()
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager!!.requestLocationPermissions(requireActivity())
+        }
+    }
+
+
+    override fun onExplanationNeeded(permissionsToExplain: List<String?>?) {
+        Toast.makeText(
+            requireContext(), "User Location Granted",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun initLocationEngine() {
+        locationEngine = LocationEngineProvider.getBestLocationEngine(requireContext())
+        val request =
+            LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+                .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+                .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME).build()
+        locationEngine!!.requestLocationUpdates(request, callback, getMainLooper())
+        locationEngine!!.getLastLocation(callback)
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        permissionsManager!!.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            mapboxMap.getStyle { style -> enableLocationComponent(style) }
+        } else {
+            Toast.makeText(requireContext(), "User Location Permission Not Granted", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (locationEngine != null) {
+            locationEngine!!.removeLocationUpdates(callback)
+        }
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
 
 }
